967c51b8e5ca43ec741605336d6c76379464e6d4
ProNinjaDev <pronin-04@bk.ru>
2025-08-23 17:44:57 +0300

    Add GET endpoint to check server status

--- main.go
+++ main.go
@@ -1,11 +1,40 @@
 package main
 
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
 type User struct {
 	Id     int64
 	Name   string
 	Status bool
 }
 
-func main() {
+func handleRoot(w http.ResponseWriter, r *http.Request) {
+	log.Println("Получен запрос")
 
+	response := map[string]string{
+		"status": "ok",
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+
+	err := json.NewEncoder(w).Encode(response)
+
+	if err != nil {
+		log.Printf("Ошибка кодирования JSON")
+	}
+}
+
+func main() {
+	http.HandleFunc("/", handleRoot)
+
+	log.Println("Запуск сервера")
+
+	err := http.ListenAndServe(":8081", nil)
+	if err != nil {
+		log.Fatalf("Не удалось запустить сервер: %v", err)
+	}
 }
